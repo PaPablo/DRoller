f201d97b761fcf00ee40dabad3a33146773948da
Papablo <pabloatm980@gmail.com>
2016-09-18 11:20:15 -0300

    Test to main and fixes

--- dados/Main.java
+++ dados/Main.java
@@ -1,13 +1,13 @@
 
-public class Test {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		if(args.length == 0){
-			System.out.println("No hay argumentos");
+			System.out.println("No arguments");
 		}
 		else if (args.length > 1) {
-			System.out.println("Mas argumentos de los validos");
+			System.out.println("Too many arguments");
 		}
 		else {
 			DieComb d = new DieComb(args[0]);
