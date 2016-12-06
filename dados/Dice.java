b6d98949d3fecd5f9757c8f894733b43aea874f5
Papablo <pabloatm980@gmail.com>
2016-12-06 11:42:06 -0300

    en camino a la mejora :)

---
+++ dados/Dice.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+//represents a single dice
+public class Dice{
+	private int sides;
+
+	/**
+	*	Constructor 
+	* @param sides number of faces of the dice
+	*/
+	public Dice(int sides){
+		this.sides = sides;
+	}
+
+	/**
+	* Default constructor, creates a D6
+	*/
+	public Dice(){
+		this(6);
+	}
+
+	public int roll(){
+	Random rdm = new Random();
+	int number = rdm.nextInt(6) + 1;
+	return number;
+	}
+
+}
--- dados/Main.java
+++ dados/Main.java
@@ -2,19 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if(args.length == 0){
-			System.out.println("No arguments");
-		}
-		else if (args.length > 1) {
-			System.out.println("Too many arguments");
-		}
-		else {
-			DieComb d = new DieComb(args[0]);
-			
-			d.roll();
-			System.out.println(d.toString());
-		}
+		
+		Dice d = new Dice(6);
+
 		
 	}
 
